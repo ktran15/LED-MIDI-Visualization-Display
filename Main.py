360af8e4bf011e64fce13afb43faca4fd3cf8039
Keith <ktran121506@gmail.com>
2026-04-21 23:15:53 -0400

    updated Main to include Midi instead of BLE

--- Main.py
+++ Main.py
@@ -1,14 +1,6 @@
-import asyncio
 import time
-import adafruit_ble_midi
-from adafruit_ble_midi import MIDIService
-import adafruit_ble
-import adafruit_midi
-from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
-from adafruit_midi.control_change import ControlChange
-from adafruit_midi.midi_message import MIDIUnknownEvent
-from adafruit_midi.note_off import NoteOff
-from adafruit_midi.note_on import NoteOn
+import mido
+import threading
 
 import numpy as np
 import rpi_ws281x as LED
@@ -22,55 +14,26 @@ strip.begin()
 
 pedal = False
 
-async def main():
+def main():
     global pedal
-    midi_service = adafruit_ble_midi.MIDIService() # creates MIDI service for BLE MIDI
-
-    ble = adafruit_ble.BLERadio() # creates BLE radio for BLE MIDI
-    if ble.connected:# just disconnects from everything if previously connected.
-        for c in ble.connections:
-            c.disconnect()
-
-    midi = adafruit_midi.MIDI(midi_out=midi_service, midi_in=midi_service, out_channel=0) # creates MIDI object for MIDI messages, sets MIDI service as input and output, and sets channel to 0
-
-    while True:
-        print("Waiting for connection to a MIDI device")
-        for advertisement in ble.start_scan(ProvideServicesAdvertisement, timeout=60):
-            if MIDIService not in advertisement.services:
-                continue
-            ble.connect(advertisement)
-            break
-
-        if ble.connections:
-            for connection in ble.connections:
-                if connection.connected and not connection.paired:
-                    print("Connected; Pairing with the MIDI device")
-                    connection.pair()
-
-            if connection.connected and connection.paired:
-                print("Paired")
-                midi_service = connection[MIDIService]
-                midi = adafruit_midi.MIDI(midi_out=midi_service, midi_in=midi_service)
-
-                while ble.connected:
-                    midi_in = midi.receive()
-                    if midi_in is None:
-                        continue
-                    if isinstance(midi_in, NoteOn):
-                        strip.setPixelColor(ledLocation(midi_in.note), LED.Color(*ledColor(midi_in.note, midi_in.velocity))) # sets color based on pitch of note
-                        strip.show()    # displays new color / brightness
-                    elif isinstance(midi_in, NoteOff):
-                        asyncio.create_task(ledFadeSustain(ledLocation(midi_in.note), ledColor(midi_in.note, midi_in.velocity), midi_in.velocity, pedal)) # calls fade and sustain function with pedal = false
-                    elif isinstance(midi_in, ControlChange):
-                        if midi_in.control == 64 and midi_in.value >= 64: # if pedal is pressed
-                            pedal = True
-                        elif midi_in.control == 64 and midi_in.value < 64: # if pedal is released
-                            pedal = False
-                    #midi_in.note = note type
-                    #midi_in.velocity = how hard the note is played
-                    #midi_in.channel = which channel the note is played on
-                    #midi_in.control =  64 for pedal
-                print("Disconnected")
+    input_port =   mido.open_input() # opens MIDI input port
+    print("Connected")
+    for msg in input_port: # loops through MIDI messages
+            if msg.type == 'note_on' and msg.velocity > 0: # if note is pressed):
+                strip.setPixelColor(ledLocation(msg.note), LED.Color(*ledColor(msg.note, msg.velocity))) # sets color based on pitch of note
+                strip.show()    # displays new color / brightness                    
+            elif msg.type == 'note_off' or (msg.type == 'note_on' and msg.velocity == 0): # if note is released
+               threading.Thread(target=ledFadeSustain, args=(ledLocation(msg.note), ledColor(msg.note, msg.velocity), msg.velocity, pedal), daemon = True).start() # calls fade and sustain function with pedal = false
+            elif msg.type == 'control_change': # if control change message (pedal)
+                if msg.control == 64 and msg.value >= 64: # if pedal is pressed
+                        pedal = True
+                elif msg.control == 64 and msg.value < 64: # if pedal is released
+                        pedal = False
+                    #msg.note = note type
+                    #msg.velocity = how hard the note is played
+                    #msg.channel = which channel the note is played on
+                    #msg.control =  64 for pedal
+    print("Disconnected")
 
 
 def veloToBrightness(velocity):
@@ -93,7 +56,7 @@ def ledColorHelper(pitch, colorIndex):
 
 def ledLocation(pitch):
     pass
-async def ledFadeSustain (ledLocation, color, velocity, pedal): # fade and sustain function once LED is pressed
+def ledFadeSustain (ledLocation, color, velocity, pedal): # fade and sustain function once LED is pressed
     (Red, Green, Blue) = color #unpacks tuple of RGB
     subR = Red / 5 # divides each color into 5 smaller steps
     subG = Green / 5 
@@ -104,7 +67,7 @@ async def ledFadeSustain (ledLocation, color, velocity, pedal): # fade and susta
         Green -= subG
         Blue -= subB
         strip.setPixelColor(ledLocation, LED.Color(int(Red), int(Green), int(Blue))) #sets new color / brightness
-        await asyncio.sleep(timeDelay) # delay between each step, so its a smooth fade
+        time.sleep(timeDelay) # delay between each step, so its a smooth fade
         strip.show() # displays new color / brightness
     strip.setPixelColor(ledLocation, LED.Color(0, 0, 0)) # makes sure goes to full black lastly
     strip.show() # displays new color / brightness
@@ -119,4 +82,4 @@ def sustainHelper(pedal, velocity):
         interFadeTime = np.interp(velocity, pedalVelo, fadeTime) # interpolates fade time based on velocity input and data lists
     return float(interFadeTime) # returns interpolated fade time value as an float
 
-asyncio.run(main())
+main()
