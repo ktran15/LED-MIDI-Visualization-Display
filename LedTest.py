07a5d5e1649beac08ca099a0a7df387b15228132
Keith <ktran121506@gmail.com>
2026-04-24 02:43:54 -0400

    updates led test

--- LedTest.py
+++ LedTest.py
@@ -1,4 +1,5 @@
 from pi5neo import Pi5Neo
+import time
 
 LED_BRIGHTNESS = 1.0
 LED_COUNT = 144
@@ -9,7 +10,7 @@ strip = Pi5Neo('/dev/spidev0.0', LED_COUNT, 800) # creates LED strip object with
 def main():
     strip.fill_strip(0, 0, 255) # sets all LEDs to blue for testing
     strip.update_strip() # displays new color / brightness
-
+    time.sleep(30)
 main()
 
 #sudo sh -c 'echo 32768 > /sys/module/spidev/parameters/bufsiz'
