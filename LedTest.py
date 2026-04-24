8eb8436a9ec11acd8247941eaef010f1f619b50e
Keith <ktran121506@gmail.com>
2026-04-24 02:39:13 -0400

    fixed test script

--- LedTest.py
+++ LedTest.py
@@ -7,7 +7,9 @@ LED_PIN = 10
 strip = Pi5Neo('/dev/spidev0.0', LED_COUNT, 800) # creates LED strip object with 288 LEDs, connected to SPI port 0.0, with a frequency of 800kHz
 
 def main():
-    strip.set_all_color(0, 0, 255) # sets all LEDs to blue for testing
+    strip.fill_strip(0, 0, 255) # sets all LEDs to blue for testing
     strip.update_strip() # displays new color / brightness
 
 main()
+
+#sudo sh -c 'echo 32768 > /sys/module/spidev/parameters/bufsiz'
