fead1b81979288e97324b1663be7ce3503a1d991
Keith <ktran121506@gmail.com>
2026-04-24 02:35:29 -0400

    test LED script

---
+++ LedTest.py
@@ -0,0 +1,13 @@
+from pi5neo import Pi5Neo
+
+LED_BRIGHTNESS = 1.0
+LED_COUNT = 144
+LED_PIN = 10
+
+strip = Pi5Neo('/dev/spidev0.0', LED_COUNT, 800) # creates LED strip object with 288 LEDs, connected to SPI port 0.0, with a frequency of 800kHz
+
+def main():
+    strip.set_all_color(0, 0, 255) # sets all LEDs to blue for testing
+    strip.update_strip() # displays new color / brightness
+
+main()
